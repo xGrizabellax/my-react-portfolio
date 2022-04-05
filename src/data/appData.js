17aa0039681364a370a342d07b5c6a80ca63adb4
Sam Page <sampage6@gmail.com>
2022-04-05 18:29:42 -0500

    added shelf care to applications

--- src/data/appData.js
+++ src/data/appData.js
@@ -1,15 +1,17 @@
-import image1 from "../images/gamejunkee-home.jpg"
-import image2 from "../images/eventube.PNG"
-import image3 from "../images/tech-blog.jpg"
-import image4 from "../images/weather-tracker.PNG"
-import image5 from "../images/daily-planner.PNG"
+import image1 from "../images/shelf-care.PNG"
+import image2 from "../images/gamejunkee-home.jpg"
+import image3 from "../images/eventube.PNG"
+import image4 from "../images/tech-blog.jpg"
+import image5 from "../images/weather-tracker.PNG"
+import image6 from "../images/daily-planner.PNG"
 
 const appData = [
-  { id: 1, title: 'GameJunkee', imageSrc: image1, url: "https://gamejunkee.herokuapp.com/" },
-  { id: 2, title: 'Eventube', imageSrc: image2, url: "https://project-1-see.github.io/eventube/" },
-  { id: 3, title: 'Tech Blog', imageSrc: image3, url: "https://sam-p-tech-blog.herokuapp.com/" },
-  { id: 4, title: 'Weather Tracker', imageSrc: image4, url: "https://xgrizabellax.github.io/hw-06-weather-tracker/" },
-  { id: 5, title: 'Daily Planner', imageSrc: image5, url: "https://xgrizabellax.github.io/hw-05-planner/" },
+  { id: 1, title: 'Shelf-Care', imageSrc: image1, url: "https://shelf-care-uhs.herokuapp.com/" },
+  { id: 2, title: 'GameJunkee', imageSrc: image2, url: "https://gamejunkee.herokuapp.com/" },
+  { id: 3, title: 'Eventube', imageSrc: image3, url: "https://project-1-see.github.io/eventube/" },
+  { id: 4, title: 'Tech Blog', imageSrc: image4, url: "https://sam-p-tech-blog.herokuapp.com/" },
+  { id: 5, title: 'Weather Tracker', imageSrc: image5, url: "https://xgrizabellax.github.io/hw-06-weather-tracker/" },
+  { id: 6, title: 'Daily Planner', imageSrc: image6, url: "https://xgrizabellax.github.io/hw-05-planner/" },
 ]
 
 export default appData;
